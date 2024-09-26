bafb46f7a3c0ea5cbb9f4aeb99490b3d3d36d446
dharani535011 <dharani535011@gmail.com>
2024-09-26 19:22:38 +0530

    update

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -13,16 +13,20 @@ const Project = () => {
         </div>
         <div className='row row-cols-1 row-cols-md-3 gap-4 pi'>
           <div className='col'>
-            <img className='img-fluid' src={img} alt="Project 1" />
+            <a href="https://nivi-interiors.netlify.app/" > <img className='img-fluid' src={img} alt="Project 1" /></a>
+            
           </div>
           <div className='col'>
-            <img className='img-fluid' src={img1} alt="Project 2" />
+            <a href="https://mrperfect-fitness-studio.netlify.app/"> <img className='img-fluid' src={img1} alt="Project 2" /></a>
+            
           </div>
           <div className='col'>
-            <img className='img-fluid' src={img2} alt="Project 3" />
+            <a href="https://magical-kheer-0ce23a.netlify.app/"> <img className='img-fluid' src={img2} alt="Project 3" /></a>
+            
           </div>
           <div className='col'>
-            <img className='img-fluid' src={img3} alt="Project 4" />
+            <a href="https://s-dharani-portfolio.netlify.app/"> <img className='img-fluid' src={img3} alt="Project 4" /></a>
+            
           </div>
         </div>
       </div>
