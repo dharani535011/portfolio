250f0883761e82330b1673d512042feebe2aab03
dharani535011 <dharani535011@gmail.com>
2024-10-07 19:43:18 +0530

    update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -9,8 +9,8 @@ const About = () => {
         <div className='a2'>
             <div className='ai'><img src={img} alt="img" /></div>
             <div className='ab'>
-                <p>I am an aspiring fullstack web Developer eager to kickstart my career in the field. Although I may not have extensive professional experience yet, I have a strong foundation in web development principles and a passion for creating engaging user experiences. I am excited to contribute my skills and enthusiasm to a dynamic team, learning and growing within a collaborative environment.</p>
-                <p> I gained hands-on experience in both frontend technologies like HTML, CSS, and JavaScript, along with popular frameworks like React, and backend technologies including Node.js , Express ,mongo db ,my sql. My goal is to create robust, user-friendly web applications that enhance user experience and performance.</p>
+                <p style={{textIndent:"70px"}}>I am an aspiring fullstack web Developer eager to kickstart my career in the field. Although I may not have extensive professional experience yet, I have a strong foundation in web development principles and a passion for creating engaging user experiences. I am excited to contribute my skills and enthusiasm to a dynamic team, learning and growing within a collaborative environment.</p>
+                <p style={{textIndent:"70px"}}> I gained hands-on experience in both frontend technologies like HTML, CSS, and JavaScript, along with popular frameworks like React, and backend technologies including Node.js , Express ,mongo db ,my sql. My goal is to create robust, user-friendly web applications that enhance user experience and performance.</p>
                 <div className='sh'>
                     <span><h4>HTML & CSS</h4><span style={{width:"50%"}}></span></span>
                     <span><h4>javaScript</h4><span style={{width:"54%"}}></span></span>
